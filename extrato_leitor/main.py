83c2296b4910686489327a811184d6328ab1b869
Glauco <gazbr234@gmail.com>
2023-05-16 00:22:47 -0300

    add new methods, some docstrings and refactoring

--- extrato_leitor/main.py
+++ extrato_leitor/main.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
 
-import os
 import re
-import pandas as pd
+from pandas import DataFrame
 from PyPDF2 import PdfReader
+from typing import Union
 
 def _token_replace(list_tokens):
     return [ re.sub('( \n)+','\t', row) for row in list_tokens ]
 
-def page_process(page_number):
-    
+def nu_credit_page_process(pdf_reader: PdfReader, page_number: int) -> Union[list, None]:
+    """ Retorna uma lista contendo dados existentes em uma determinada página
+    de um extrato de cartão em PDF.
+
+    Args:
+        pdf_reader (_type_): Objeto do pacote PyPDF2 que controla a leitura
+        do PDF.
+        page_numer (int): número da página no arquivo.
+
+    Raises:
+        ValueError: 
+
+    Returns:
+        list: contendo dados de uma página
+    """
+
     if page_number < len(pdf_reader.pages):
         page_text = pdf_reader.pages[page_number].extract_text()
         expr2 = "[0-3][0-9] [A-Z]{3} \n \n[\w*/ \-.]+ \n(?:\.*[0-9]{1,3})+\,[0-9]{2} \n"
@@ -19,11 +33,36 @@ def page_process(page_number):
         print('Invalid page number')
         raise ValueError
 
+def parse_table(pdf_reader: PdfReader) -> DataFrame:
+    """ Esse método monta uma tabela com dados de extrado através de um 
+    DataFrame do Pandas, a partir de um layout em pdf pré-definido, aqui 
+    processado pelo método `page_process`.
+
+    Args:
+        pdf_reader (PdfReader): Objeto do pacote PyPDF2 que controla a leitura
+        do PDF.
+
+    Returns:
+        DataFrame: Tabela para ser exportada.
+    """
+    
+    new_doc = []
+    for i in range(START_PAGE, len(pdf_reader.pages)):
+        print(f'Lendo página: {i}')
+        new_doc += nu_credit_page_process(pdf_reader, page_number=i)
+    
+    extrato_df = DataFrame(
+    [row.split('\t') for row in new_doc], 
+    columns=['Date','Shop Name', 'Value (R$)', 'to_be_dropped'])\
+        .drop(['to_be_dropped'], axis=1)\
+        .dropna(axis=0, how='all')
+    
+    return extrato_df
 
 if __name__ == "__main__":
 
     VERSION=0.1
-    FILEPATH='fatura.pdf'
+    FILEPATH='faturas/fatura.pdf'
     START_PAGE=3
     
     print(f"Leitor de Extratos - {VERSION}")
@@ -37,16 +76,8 @@ if __name__ == "__main__":
         new_name=FILEPATH
 
     pdf_reader = PdfReader(new_name)
-    
-    new_doc = []
-    for i in range(START_PAGE, len(pdf_reader.pages)):
-        print(f'Lendo página: {i}')
-        new_doc += page_process(page_number=i)
-    
-    extrato_df = pd.DataFrame(
-    [row.split('\t') for row in new_doc], 
-    columns=['Date','Shop Name', 'Value (R$)', 'to_be_dropped'])\
-        .drop(['to_be_dropped'], axis=1)\
-        .dropna(axis=0, how='all')
-    
+    extrato_df = parse_table(pdf_reader)
+
+    print(f"Tabela gerada: {extrato_df.shape[0]} linhas obtidas")
+
     extrato_df.to_csv(f'{new_file_path.replace(".pdf","")}_extracted.csv', sep=';', index=False)
